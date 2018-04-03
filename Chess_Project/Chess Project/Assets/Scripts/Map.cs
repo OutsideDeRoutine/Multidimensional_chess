6e3ee56b49edfb4a56aee3926a1067adc0880a1a
Fernando Palazuelo <DatFerPa@users.noreply.github.com>
2018-04-03 22:23:27 +0200

    Prototipo para crear personajes
    prototipo para la creación de personajes.,

--- Chess_Project/Chess Project/Assets/Scripts/Map.cs
+++ Chess_Project/Chess Project/Assets/Scripts/Map.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map : MonoBehaviour {
+public class Map : MonoBehaviour
+{
 
 
-
+    //patrón para la creación del tablero
     private string sideform = "-----  --|" +
                               "  -----  |" +
                               "--  -----";
 
-    private int zTiles=5;
+    //tamaño del tile
+    private int zTiles = 5;
+    //diferencias de alturas entre tiles
     private int yDif = 5;
-
+    private IDictionary<String,GameObject> tiles;
 
     // Use this for initialization
-    void Start () {
-        GenerateMap(sideform.Split('|'));
+    void Start()
+    {
+        tiles = new Dictionary<String,GameObject>();
+        GenerateMap(sideform.Split('|'));       
     }
 
-    void GenerateMap (string[] platforms)
+    //metodo para generar los tiles del tablero
+    void GenerateMap(string[] platforms)
     {
         int y = 0;
         int x = 0;
@@ -27,20 +34,54 @@ public class Map : MonoBehaviour {
         {
             foreach (char plat in platform)
             {
-                if(plat!=' ')
+                if (plat != ' ')
                 {
-                    for(int z=0; z< zTiles; z++)
+                    for (int z = 0; z < zTiles; z++)
                     {
+                        //En Cube se guardan los TileData
                         GameObject tile = Instantiate(Resources.Load("Cube", typeof(GameObject))) as GameObject;
-                        tile.transform.position = new Vector3(x, y * -yDif, z);
+                        //definimos posicion fisica de los tiles
+                        tile.transform.position = new Vector3(x, y * -yDif, z);   
+                        //definimos posicion lógica de los tiles (se guarda dentro de TileData)
                         tile.GetComponent<TileData>().setPosition(x, y, z);
+                        tile.name = "Tile_" + tile.GetComponent<TileData>().position.x +"-"+ tile.GetComponent<TileData>().position.z +"_nivel:"+ tile.GetComponent<TileData>().position.y;
+                        //añadimos el tile a la 
+                        String key = "tile" + tile.GetComponent<TileData>().position.x + "-" + tile.GetComponent<TileData>().position.z + "-" + tile.GetComponent<TileData>().position.y;
+                        tiles.Add(key,tile);
                     }
                 }
                 x++;
             }
-            y ++;
+            y++;
             x = 0;
         }
+        generateCharacters();
     }
-	
+
+    //Método para generar a los personajes jugables sobre los tiles del tablero
+    private void generateCharacters()
+    {
+        /*
+         Cosas a quizá tener en cuenta:
+            usar un prefab para las piezas (de manera general) y luego especificarlas en codigo
+            definir un mouseClick o algo parrecido luego para las fichas
+         
+            suicidio :)
+         */
+
+
+        //crear personajes circulares (de mientras) 
+        GameObject charPorDefecto = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        charPorDefecto.transform.localScale = new Vector3(0.7F , 0.7F,0.7F);
+        //probando generacion simple en una celda determinada
+        //primero sacamos el tile, tendremos que sacarlos a pelo para la generación de las piezas
+        //no tiene que ser chapucero por que se colocan de esa manera y no permitimos diferentes tableros
+        GameObject tileAux = tiles["tile1-2-0"];
+
+        charPorDefecto.transform.position = new Vector3(tileAux.transform.position.x, tileAux.transform.position.y + 0.8F, tileAux.transform.position.z);
+
+
+
+    }
+
 }
